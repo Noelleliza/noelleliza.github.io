b139143d7d1d57ec7518f5288a353b6192245f13
Timothy B. Smith <smith@ttimsmith.com>
2017-06-27 10:29:06 -0500

    Fixing hamburger menu… kinda

--- javascript/tools.js
+++ javascript/tools.js
@@ -7,41 +7,41 @@ var close = document.getElementById("closebtn");
 
 //default to measure if/else from
 nav.style.height = "50px";
-main.style.marginTop = "50px";
+main.style.marginTop = "0px";
 for (i = 0; i < menu.length; i++){menu[i].style.marginTop="100px";};
 
 close.addEventListener("click", function(){
   var menuIcon = close.children;
   for (i = 0; i < menuIcon.length; i++){
   menuIcon[i].classList.toggle("active");
-  }   
+  }
 });
 
-function navToggle() {  
+function navToggle() {
   //to close
   if (nav.style.height <= "275px") {
   nav.style.height = "50px";
-  main.style.marginTop = "50px";
-  
+  main.style.marginTop = "0px";
+
       var i = 0;
       for (i = 0; i < menu.length; i++){
   menu[i].style.opacity="0.0";
   menu[i].style.marginTop="100px";
   };
       document.body.style.backgroundColor = "rgba(0,0,0,0.0)";
-  
-  } 
+
+  }
   //to open
   else if (nav.style.height <= "50px") {
   nav.style.height = "275px";
-  main.style.marginTop = "275px";
+  main.style.marginTop = "225px";
       var i = 0;
       for (i = 0; i < menu.length; i++){
   menu[i].style.opacity="1.0";
   menu[i].style.marginTop="0px";
   };
       // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-  
+
   }
 
 };
